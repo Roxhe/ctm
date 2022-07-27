dc8d270739f48a841b1ae7e7f10c6d208af66588
Roxhe <88476983+Roxhe@users.noreply.github.com>
2022-07-27 17:41:21 +0200

    simplification round

--- Models/model_round.py
+++ Models/model_round.py
@@ -4,18 +4,18 @@ class Round1():
 
     def __init__(self, list_players):
         self.list_players = list_players
-        self.pair_list = []
         self.played_match = []
 
-    def create_pair(self, list_players, pair_list):
-        tuple_pair = []
+    def create_pair(self, list_players):
+        pair_list = []
+
         list_players_h1 = list_players[:len(list_players) // 2]
         list_players_h2 = list_players[len(list_players) // 2:]
         for i, j in zip(list_players_h1, list_players_h2):
-            tuple_pair.append(((i, 0), (j, 0)))
-        for k in range(len(tuple_pair)):
-            print(dict(tuple_pair[k]))
-            pair_list.append(dict(tuple_pair[k]))
+            i = (i, 0)
+            j = (j, 0)
+            k = (i, j)
+            pair_list.append(dict(k))
         return pair_list
 
 
@@ -24,6 +24,6 @@ class Round1():
 list_joueur_test = [1, 2, 3, 4, 5, 6, 7, 8]
 
 round1 = Round1(list_joueur_test)
-print(round1.create_pair(list_joueur_test, pair_list=[]))
+print(round1.create_pair(list_joueur_test))
 
 
